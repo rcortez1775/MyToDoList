c560265c3fdb971f99268fb096c4c19dcd60a073
jameel <jameel.matin@gmail.com>
2018-12-02 12:35:57 -0800

    fixed the add button

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,72 +1,84 @@
-//Variables used to manipulate DOM elements
-var input = document.querySelector("input[type='text']");
-var ul = document.querySelector("ul");
-var container = document.querySelector("div");
-var lists = document.querySelectorAll("li");
-var spans = document.getElementsByTagName("span");
-var addBtn = document.querySelector(".add");
-var saveBtn = document.querySelector(".save");
-var clearBtn = document.querySelector(".clear");
-
+//letiables used to manipulate DOM elements
+let input = document.querySelector("input[type='text']");
+let ul = document.querySelector("ul");
+let container = document.querySelector("div");
+let lists = document.querySelectorAll("li");
+let spans = document.getElementsByTagName("span");
+let addBtn = document.querySelector(".add");
+let saveBtn = document.querySelector(".save");
+let clearBtn = document.querySelector(".clear");
 
 //This function deletes the todo if the delete span is clicked
 function deleteTodo() {
-    for (let span of spans) {
-        span.addEventListener("click", function () {
-            event.stopPropagation();
-        });
-    }
+  for (let span of spans) {
+    span.addEventListener("click", function() {
+      event.stopPropagation();
+    });
+  }
 }
 
 //This function loads the todo if a list is found in local storage
 function loadTodo() {
-    if (localStorage.getItem('todoList')) {
-        ul.innerHTML = localStorage.getItem('todoList');
-        deleteTodo();
-    }
+  if (localStorage.getItem("todoList")) {
+    ul.innerHTML = localStorage.getItem("todoList");
+    deleteTodo();
+  }
 }
 
 //This function adds user input to the todo list
-/*addBtn.addEventListener('click', function () {
-        var li = document.createElement("li");
-        var spanElement = document.createElement("span");
-        var newTodo = this.input;
-        this.input = " ";
-        ul.appendChild(li).append(spanElement, newTodo);
-        deleteTodo();
-}); */
+addBtn.addEventListener("click", function() {
+  // let li = document.createElement("li");
+  // let spanElement = document.createElement("span");
+  // let newTodo = this.input;
+  // this.input = " ";
+  // ul.appendChild(li).append(spanElement, newTodo);
+  // let newTodo = input.value;
+  // deleteTodo();
 
-
+  let li = document.createElement("li");
+  let spanElement = document.createElement("span");
+  const input = document.querySelector("input");
+  let newTodo = input.value;
+  this.input = " ";
+  ul.appendChild(li).append(spanElement, newTodo);
+  deleteTodo();
+});
 
 //This event listener will add any new text to the list when the enter key is pressed
-input.addEventListener("keypress", function (keyPressed) {
-    if (keyPressed.which === 13) {
-        var li = document.createElement("li");
-        var spanElement = document.createElement("span");
-        var newTodo = this.value;
-        this.value = " ";
-        ul.appendChild(li).append(spanElement, newTodo);
-        deleteTodo();
-    }
+input.addEventListener("keypress", function(keyPressed) {
+  if (keyPressed.which === 13) {
+    let li = document.createElement("li");
+    let spanElement = document.createElement("span");
+
+    let newTodo = this.value;
+    this.value = " ";
+
+    ul.appendChild(li).append(spanElement, newTodo);
+
+    deleteTodo();
+  }
 });
 
 //This event listener changes the font decoration to linethrough when item is clicked
-ul.addEventListener('click', function (ev) {
-    if (ev.target.tagName === 'LI') {
-        ev.target.classList.toggle('checked');
+ul.addEventListener(
+  "click",
+  function(ev) {
+    if (ev.target.tagName === "LI") {
+      ev.target.classList.toggle("checked");
     }
-}, false);
+  },
+  false
+);
 
 //This event listener will save the todolist state when the save button is clicked
-saveBtn.addEventListener('click', function () {
-    localStorage.setItem('todoList', ul.innerHTML);
-
+saveBtn.addEventListener("click", function() {
+  localStorage.setItem("todoList", ul.innerHTML);
 });
 
 //This event listener will clear the todolist when the clear button is clicked
-clearBtn.addEventListener('click', function () {
-    ul.innerHTML = "";
-    localStorage.removeItem('todoList', ul.innerHTML);
+clearBtn.addEventListener("click", function() {
+  ul.innerHTML = "";
+  localStorage.removeItem("todoList", ul.innerHTML);
 });
 
 //delete todo
